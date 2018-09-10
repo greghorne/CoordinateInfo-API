2c23b81a0fd71db78d03692251c58a09c9da89f0
Greg Horne <greghorne@hotmail.com>
2018-09-10 19:01:43 +0000

    corrected rack-attack error

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -3,13 +3,17 @@ class Rack::Attack
     Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new 
 
     throttle('req/ip', :limit => 100, :period => 120.seconds) do |req|
-        Rails.logger.error("Rack::Attack Too many request attempts from IP: #{req.ip}; Limit 100 requests per minute.")
+        # Rails.logger.error("Rack::Attack Too many request attempts from IP: #{req.ip}; Limit 100 requests per minute.")
         req.ip 
     end
 
-    # throttle('req/ip', :limit => 3, :period => 1.seconds) do |req|
-    #     Rails.logger.error("Rack::Attack Too many requests per second from IP: #{req.ip}; Limit 3 requests per second.")
-    #     req.ip 
-    # end
+    self.throttled_response = ->(env) {
+        retry_after = (env['rack.attack.match_data'] || {})[:period]
+        [
+          429,
+          {'Content-Type' => 'application/json', 'Retry-After' => retry_after.to_s},
+          [{error: "Too many request attempts.  Limit 100 requests per 2 minutes."}.to_json]
+        ]
+      }
 
 end
