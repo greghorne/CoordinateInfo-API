b2cd0eb34aba505a334af737a6bee052e26554bd
Greg Horne <greghorne@hotmail.com>
2018-09-11 16:02:09 +0000

    commented out all of the code, for now

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,8 @@
-$redis = Redis::Namespace.new("coord_info", :redis => Redis.new)
+# $redis = Redis::Namespace.new("coord_info", :redis => Redis.new)
 #Redis.current = Redis.new(:host => '<VM IP>', :port => <VM PORT>, :password => '<PASSWORD>')
+# $redis = Redis.new(host = '192.168.1.120', port = 6379, db = 15)
+# $redis = Redis::Namespace.new(:host => '192.168.1.120', :port => 6379, db: "coord_info")
+# require 'redis'
+# $redis = Redis.new(host: "192.168.1.120", port: 6379, db: 15)
+
+# $redis = Redis::Namespace.new("coord_info", :redis => Redis.new, :host => '192.168.1.120')
