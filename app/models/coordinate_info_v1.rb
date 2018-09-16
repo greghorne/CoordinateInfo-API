88e754e8943e5b2ba97185124b451111c74473c2
Greg Horne <greghorne@hotmail.com>
2018-09-16 20:19:21 +0000

    closed opened mongo connection

--- app/models/coordinate_info_v1.rb
+++ app/models/coordinate_info_v1.rb
@@ -177,6 +177,7 @@ class CoordinateInfoV1 < ApplicationRecord
                     collection = conn["gadm36"]
 
                     response_cursor = collection.find({"geometry":{"$geoIntersects":{"$geometry":{"type":"Point", "coordinates":[longitude_x.to_f, latitude_y.to_f]}}}})
+                    conn.close
 
                     # some weirdness here; check on how to do it properly
                     document = ""
