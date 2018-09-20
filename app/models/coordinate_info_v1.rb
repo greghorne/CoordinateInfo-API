02c5e1fbfadf6dfde6ac6d355159c463fe2e154b
Greg Horne <greghorne@hotmail.com>
2018-09-20 18:08:18 +0000

    changed mongo connect to 'secondary_preferred'

--- app/models/coordinate_info_v1.rb
+++ app/models/coordinate_info_v1.rb
@@ -107,7 +107,7 @@ class CoordinateInfoV1 < ApplicationRecord
                         conn_string = $db_host_mongo.to_s + ":" + $db_port_mongo.to_s
                     end
                     # valid values are :primary, :primary_preferred, :secondary, :secondary_preferred and :nearest
-                    conn = Mongo::Client.new([conn_string], :database => $db_name_mongo, :user => $db_user_mongo, :password => $db_pwd_mongo, :read => { :mode => :nearest })
+                    conn = Mongo::Client.new([conn_string], :database => $db_name_mongo, :user => $db_user_mongo, :password => $db_pwd_mongo, :read => { :mode => :secondary_preferred })
                     return conn
 
                 rescue
